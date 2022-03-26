fb2457ad7bf9808a17b85e01a518562ab7f7df7a
sean-rohan-NOAA <sean.rohan@noaa.gov>
2022-03-25 17:34:24 -0700

    Changes to code to resolve (#2)

--- functions/sequentialOR.R
+++ functions/sequentialOR.R
@@ -5,11 +5,12 @@
 #' 
 #' @param data Data frame containing predictor and response variables.
 #' @param method Character vector of length one indicating which function should be used to fit models. Options are 'lm', 'glm', or 'gam'.
-#' @param fn Formula to be passed to the model. Formula must be valid for the selected method. Variables must included in `data` and the extraction operator ($) should not be used in the forumla.
+#' @param formula Formula to be passed to the model. Formula must be valid for the selected method. Variables must included in `data` and the extraction operator ($) should not be used in the forumla.
 #' @param n.reject Numerical vector of length one. Number of observations to be rejected in each iteration. Larger values speed the funciton up, but may impede precise detection of break points.
 #' @param n.stop Numerical vector of length one which indicating the number of observations remaining or proportion of initial observations which should be remaining before outlier rejection stops. For example, with 400 initial observations, n.stop set to 40 or 0.1 (= 400*0.1) would stop the algorithm when 40 observations remain.
+#' @param threshold.stop Optional. Threshold value for stopping sequentialOR as a 1L numeric vector. If provided, ends sequentialOR calculations when the maximum absolute residual is less than the threshold.stop.
 #' @param tail Character vector of length one indicating whether to reject from the lower tail, upper tail, or both tails. Default = "both".
-#' #' @param plot Logical vector indicating whether the function should print a plot of observations versus RMSE.
+#' @param plot Logical vector indicating whether the function should print a plot of observations versus RMSE.
 #' @param ... Additional arguments passed to function calls (family, offset, etc.)
 #'
 #' @return The function returns a list with (1) input data frame with an additional column indicating the order in which observations were rejected, (2) a data frame containing the number of observations used for model-fitting and the associated RMSE. If argument `plot=T`, also prints a plot of observations versus RMSE given the remaining observations.
@@ -39,7 +40,8 @@ sequentialOR <- function(data, method = 'lm', formula, n.reject = 1, n.stop, thr
 
   # Rejection counter
   rejection <- 1
-
+  
+  
   for(i in 1:iter) {
 
     # Subset data that hasn't been rejected
@@ -71,7 +73,7 @@ sequentialOR <- function(data, method = 'lm', formula, n.reject = 1, n.stop, thr
     if(tail == "lower") data$SOR_RANK[which(data$index == data.sub$index[which.min(data.sub$resid)])] <- i
 
     # Calculate RMSE for iteration
-    RMSE[i] <- mean(residuals(mod)^2)
+    RMSE[i] <- mean(sqrt(residuals(mod)^2))
     NN[i] <- i
     PARSLOPE[i] <- coef(mod)[2]
 
@@ -80,7 +82,7 @@ sequentialOR <- function(data, method = 'lm', formula, n.reject = 1, n.stop, thr
       if(max(abs(resid(mod))) < threshold.stop) {
         RMSE <- RMSE[1:i]
         NN <- NN[1:i]
-        print(paste0("Stopping threshold reached. Stopped after iteration " , iter))
+        print(paste0("Stopping threshold reached. Stopped after iteration " , i))
         return(list(obs_rank = data, rmse = data.frame(N = NN, RMSE = RMSE)))
       }
     }
