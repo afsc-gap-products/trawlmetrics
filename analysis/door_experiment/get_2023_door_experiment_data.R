0547739306f5a7cfb61b3565f6baae529222991a
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-02-25 11:22:27 -0800

    Add historical tows to door experiment query

--- analysis/door_experiment/get_2023_door_experiment_data.R
+++ analysis/door_experiment/get_2023_door_experiment_data.R
@@ -1,5 +1,5 @@
 # Net width and height data from door comparison experiments
-# February 22, 2024
+# February 25, 2024
 # Sean Rohan <sean.rohan@noaa.gov>
 
 # remotes::install_github(repo = "afsc-gap-products/postsurvey_hauldata_processing")
@@ -7,7 +7,7 @@ install.packages("plotly")
 
 library(trawlmetrics)
 
-# Get data (net width, net height, events, and haul) ----
+# Get data from experimental tow stations (net width, net height, events, and haul) ----
 
 channel <- trawlmetrics::get_connected(schema = "AFSC")
 
@@ -69,6 +69,76 @@ events <- RODBC::sqlQuery(channel = channel,
 hauls <- RODBC::sqlQuery(channel = channel,
                          query = "select * from racebase.haul where vessel = 162 and cruise = 202301 and haul_type = 7")
 
+
+# Get data from standard hauls at experimental tow stations ----
+
+# Contains means and standard deviation for height and spread; spread method 7 = Width from Marport after sequential outlier rejection
+
+standard_hauls <- RODBC::sqlQuery(channel = channel,
+                                     query = paste0("select * from race_data.hauls 
+                                                 where station in ('", paste(unique(hauls$STATIONID), collapse = "', '"), "')
+                                                 and performance >= 0 
+                                                 and haul_type = 3 
+                                                 and net_spread_method = 7"))
+
+standard_width <- RODBC::sqlQuery(channel = channel,
+                                  query = paste0("select nm.date_time, nm.value net_width, h.haul_id 
+                         from race_data.net_mensurations nm, race_data.net_mensuration_headers nmh, race_data.hauls h
+                         where nm.net_mensuration_header_id = nmh.net_mensuration_header_id 
+                         and h.haul_id in (", paste(standard_hauls$HAUL_ID, collapse = ", "), 
+                         ") and h.haul_id = nmh.haul_id 
+                         and nmh.data_type_id = 2 
+                         and nm.value > 10 
+                         and nm.value < 22")) |>
+  dplyr::mutate(DATE_TIME = lubridate::with_tz(
+    lubridate::force_tz(DATE_TIME, tzone = "UTC"), 
+    tzone = "America/Anchorage")
+  ) |>
+  dplyr::arrange(DATE_TIME)
+
+standard_height <- RODBC::sqlQuery(channel = channel,
+                                  query = paste0("select nm.date_time, nm.value net_width, h.haul_id 
+                         from race_data.net_mensurations nm, race_data.net_mensuration_headers nmh, race_data.hauls h
+                         where nm.net_mensuration_header_id = nmh.net_mensuration_header_id 
+                         and h.haul_id in (", paste(standard_hauls$HAUL_ID, collapse = ", "), 
+                                                 ") and h.haul_id = nmh.haul_id 
+                         and nmh.data_type_id = 3 
+                         and nm.value > 10 
+                         and nm.value < 22")) |>
+  dplyr::mutate(DATE_TIME = lubridate::with_tz(
+    lubridate::force_tz(DATE_TIME, tzone = "UTC"), 
+    tzone = "America/Anchorage")
+  ) |>
+  dplyr::arrange(DATE_TIME)
+
+standard_events <- RODBC::sqlQuery(channel = channel,
+                                   query = paste0("select e.haul_id, e.date_time, e.event_type_id, et.name, h.haul, c.vessel_id vessel, c.cruise 
+                          from race_data.events e, race_data.event_types et, race_data.hauls h, race_data.cruises c, race_data.surveys s 
+                          where e.haul_id = h.haul_id 
+                          and e.haul_id in (", paste(standard_hauls$HAUL_ID, collapse = ", "),
+                          ") and h.cruise_id = c.cruise_id 
+                          and c.survey_id = s.survey_id 
+                          and et.event_type_id = e.event_type_id 
+                          and e.event_type_id in (2, 3, 5, 6, 7, 8, 9)")) |>
+  dplyr::mutate(DATE_TIME = lubridate::with_tz(
+    lubridate::force_tz(DATE_TIME, tzone = "UTC"), 
+    tzone = "America/Anchorage")
+  ) |>
+  dplyr::arrange(DATE_TIME)
+
+standard_rb_hauls <- RODBC::sqlQuery(channel = channel,
+                                  query = paste0("select * from racebase.haul h 
+                                                 where performance >= 0 
+                                                 and haul_type = 3 
+                                                 and stationid in ('", paste(unique(hauls$STATIONID), collapse = "', '"), "')")) |>
+  dplyr::inner_join(
+    unique(
+      dplyr::select(standard_events, VESSEL, CRUISE, HAUL, HAUL_ID)
+      )
+    )
+
+
+# Sequential outlier rejection on width for experimental hauls
 unique_hauls <- unique(events$HAUL_ID)
 
 sor_width <- data.frame()
@@ -135,27 +205,3 @@ for(jj in 1:length(unique_hauls)) {
   readline("Press Enter to advance to the next plot.")
 
 }
-
-
-# continue <- "n"
-# 
-# sel_width$COMMENT <- NA
-# sel_width$col <- "black"
-# 
-# plot(x = sel_width$DATE_TIME, 
-#      y = sel_width$NET_WIDTH, 
-#      col = sel_width$col,
-#      pch = 19,
-#      xlab = "Time", 
-#      ylab = "Width (m)", 
-#      main = paste0("Haul: ", sel_events$HAUL[1]))
-# 
-# 
-# plot(x = sel_width$DATE_TIME, 
-#      y = sel_width$NET_WIDTH, 
-#      col = sel_width$col,
-#      pch = 19,
-#      xlab = "Time", 
-#      ylab = "Width (m)", 
-#      main = paste0("Haul: ", sel_events$HAUL[1]),
-#      type = 'l')
