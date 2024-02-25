62b716904a0aeb2982168548d75d3316bd85af81
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-02-25 11:59:06 -0800

    Add time series plots of spread and scope-to-depth ratio

--- analysis/door_experiment/get_2023_door_experiment_data.R
+++ analysis/door_experiment/get_2023_door_experiment_data.R
@@ -3,11 +3,14 @@
 # Sean Rohan <sean.rohan@noaa.gov>
 
 # remotes::install_github(repo = "afsc-gap-products/postsurvey_hauldata_processing")
-install.packages("plotly")
+# install.packages("plotly", "ggthemes")
 
 library(trawlmetrics)
+library(ggthemes)
 
 # Get data from experimental tow stations (net width, net height, events, and haul) ----
+
+dir.create(here::here("analysis", "door_experiment", "plots"))
 
 channel <- trawlmetrics::get_connected(schema = "AFSC")
 
@@ -75,11 +78,13 @@ hauls <- RODBC::sqlQuery(channel = channel,
 # Contains means and standard deviation for height and spread; spread method 7 = Width from Marport after sequential outlier rejection
 
 standard_hauls <- RODBC::sqlQuery(channel = channel,
-                                     query = paste0("select * from race_data.hauls 
-                                                 where station in ('", paste(unique(hauls$STATIONID), collapse = "', '"), "')
-                                                 and performance >= 0 
-                                                 and haul_type = 3 
-                                                 and net_spread_method = 7"))
+                                     query = paste0("select h.*, c.vessel_id vessel, c.cruise from race_data.hauls h, race_data.cruises c 
+                                                 where h.station in ('", paste(unique(hauls$STATIONID), collapse = "', '"), "')
+                                                 and h.performance >= 0 
+                                                 and h.haul_type = 3 
+                                                 and h.net_spread_method = 7 
+                                                 and h.cruise_id = c.cruise_id")) |>
+  dplyr::arrange(CRUISE)
 
 standard_width <- RODBC::sqlQuery(channel = channel,
                                   query = paste0("select nm.date_time, nm.value net_width, h.haul_id 
@@ -136,7 +141,6 @@ standard_rb_hauls <- RODBC::sqlQuery(channel = channel,
       dplyr::select(standard_events, VESSEL, CRUISE, HAUL, HAUL_ID)
       )
     )
-
 
 # Sequential outlier rejection on width for experimental hauls
 unique_hauls <- unique(events$HAUL_ID)
@@ -205,3 +209,46 @@ for(jj in 1:length(unique_hauls)) {
   readline("Press Enter to advance to the next plot.")
 
 }
+
+
+# Plot standard haul average width and standard deviation
+ggplot() +
+  geom_point(data = standard_hauls,
+             mapping = aes(x = STATION, y = NET_SPREAD)) +
+  scale_y_continuous(name = "Net spread (m)") +
+  scale_x_discrete(name = "Station")
+
+plot_width_timeseries <- ggplot() +
+  geom_hline(data = standard_hauls |>
+               dplyr::group_by(STATION) |>
+               dplyr::summarise(MEAN_SPREAD = mean(NET_SPREAD)), 
+             mapping = aes(yintercept = MEAN_SPREAD, color = STATION),
+             linetype = 2) +
+  geom_path(data = standard_hauls,
+            mapping = aes(x = floor(CRUISE/100),
+                          y = NET_SPREAD, 
+                          color = STATION)) +
+  geom_errorbar(data = standard_hauls,
+                mapping = aes(x = floor(CRUISE/100),
+                              ymin = NET_SPREAD-2*NET_SPREAD_STANDARD_DEVIATION,
+                              ymax = NET_SPREAD+2*NET_SPREAD_STANDARD_DEVIATION,
+                              color = STATION), width = 0.1) +
+  geom_point(data = standard_hauls,
+             mapping = aes(x = floor(CRUISE/100),
+                           y = NET_SPREAD,
+                           color = STATION)) +
+  geom_text(data = standard_hauls,
+            mapping = aes(x = floor(CRUISE/100),
+                          y = NET_SPREAD + 2*NET_SPREAD_STANDARD_DEVIATION,
+                          color = STATION,
+                          label = round(WIRE_OUT/BOTTOM_DEPTH, 2)), hjust = -0.15) +
+  scale_y_continuous(name = "Net spread (m)") +
+  scale_x_continuous(name = "Year", limits = c(2012, 2025)) + 
+  scale_color_colorblind(name = "Station") +
+  facet_wrap(~STATION, nrow = 4) +
+  theme_bw()
+
+
+png(filename = here::here("analysis", "door_experiment", "plots", "width_scope2depth_timeseries.png"), width = 169, height = 200, units = "mm", res = 300)
+print(plot_width_timeseries)
+dev.off()
