a97694c69533234f5262b09501cbac45132902fe
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-05-09 07:00:24 -0700

    Examples and 2024 EBS scripts

---
+++ doc/ex_sor_ai_148_202201.R
@@ -0,0 +1,57 @@
+library(trawlmetrics)
+
+# 2022 AI Ocean Explorer ---------------------------------------------------------------------------
+cruise1 = 202201
+cruise_idnum1 = 754
+vessel1 = 148
+region1 = "AI"
+survey1 = "AI_2022"
+width_range1 = c(10, 22)
+create_user = "ROHANS"
+fill_method = "goa"  # Uses GAMs to fill height and spread when data are missing or too sparse.
+convert_marport_to_netmind = FALSE # No Marport to Netmind conversion
+
+# Retrieve haul and net mensuration data from race_data then write spread and height data from individual hauls to the [subdirectory]: /output/{region}/{cruise}/{vessel}.
+# - Height: [subdirectory]/ping_files_{survey}/HEIGHT_{region}_{cruise}_{vessel}.rds
+# - Hauls: [subdirectory]/ping_files_{survey}/{cruise}_{vessel}_{haul}_pings.rds
+sor_setup_directory(cruise = cruise1, # cruise number
+                    cruise_idnum = cruise_idnum1,
+                    vessel = vessel1,
+                    region = region1,
+                    survey = survey1)
+
+# Run sequential outlier rejection on rds files from each haul and write outputs to .rds files.
+# Hauls w/ SOR: [subdirectory]/ping_files_{survey}/{cruise}_{vessel}_{haul}_sor.rds
+sor_run(cruise = cruise1,
+        vessel = vessel1,
+        region = region1,
+        survey = survey1,
+        overwrite = TRUE)
+
+# Plot results of sequential outlier rejection for visual inspection.
+# Plots: [subdirectory]/ping_files_{survey}/SOR_graphics_{survey}/SOR_{cruise}_{vessel}_{haul}.png
+sor_plot_results(cruise = cruise1,
+                 vessel = vessel1,
+                 region = region1,
+                 survey = survey1)
+
+# Fill in missing height data using the mean height for a given scope then estimate missing spread using a GLM. Write results to .rds files.
+# Hauls w/ missing data filled: [subdirectory]/ping_files_{survey}/{cruise}_{vessel}_{haul}_final.rds
+sor_fill_missing(height_paths = here::here("output", region1, cruise1, vessel1, 
+                                           paste0("HEIGHT_", region1, "_", cruise1, "_", vessel1, ".rds")),
+                 spread_paths = here::here("output", region1, cruise1, vessel1, 
+                                           paste0("SPREAD_AFTER_SOR_", region1, "_", cruise1, "_", vessel1, ".rds")),
+                 haul_path = here::here("output", region1, cruise1, vessel1, 
+                                        paste0("edit_haul_", cruise1, "_", vessel1, ".rds")),
+                 rds_dir = here::here("output", region1, cruise1, vessel1, 
+                                      paste0("PING_FILES_", region1, "_", floor(cruise1/100))),
+                 fill_method = fill_method,
+                 convert_marport_to_netmind = convert_marport_to_netmind)
+
+
+# Add updated data to Oracle and write output to .csv
+sor_save_results(final_dir = here::here("output", region1, cruise1, vessel1, 
+                                        paste0("PING_FILES_", region1, "_", floor(cruise1/100))), 
+                 create_user = create_user, 
+                 survey = survey1, 
+                 cruise_idnum = cruise_idnum1)
